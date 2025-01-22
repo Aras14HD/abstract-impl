98f41b6bcf5970d06f2b81161890430092e568bd
Aras14HD <aras14k5@gmail.com>
2025-01-22 18:19:40 +0100

    add basic demands

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use quote::ToTokens;
+use syn::punctuated::Punctuated;
+use syn::token;
+use syn::{parse_macro_input, ItemImpl};
+mod change_self;
+mod dummy;
+mod mac;
+mod transform;
+
+struct IdentList(Punctuated<syn::Ident, token::Comma>);
+impl syn::parse::Parse for IdentList {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(IdentList(
+            input.parse_terminated(syn::Ident::parse, token::Comma)?,
+        ))
+    }
+}
+
+/// Define an abstract implementation for a trait, that types can use
+///
+/// ```
+/// # use abstract_impl::abstract_impl;
+/// # trait SomeTrait {
+/// #   fn some() -> Self;
+/// #   fn other(&self);
+/// # }
+/// #[abstract_impl]
+/// impl SomeTrait for Impl where Self: Default + std::fmt::Debug {
+///   fn some() -> Self {
+///     Self::default()
+///   }
+///   fn other(&self) {
+///     println!("{context:?}")
+///   }
+/// }
+/// # fn main() {}
+/// ```
+#[proc_macro_attribute]
+pub fn abstract_impl(
+    _attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let parsed = parse_macro_input!(item as ItemImpl);
+    let IdentList(attrs) = parse_macro_input!(_attr);
+    let res = transform::transform(
+        parsed,
+        attrs.iter().all(|attr| attr.to_string() != "no_dummy"),
+        attrs.iter().all(|attr| attr.to_string() != "no_macro"),
+    );
+    res.to_token_stream().into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
