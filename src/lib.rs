9829470b76177d126d2ee5a186b2adc498d7fb7d
Aras14HD <aras14k5@gmail.com>
2025-01-30 20:30:53 +0100

    add use_type macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@
 #![doc = include_str!("../README.md")]
 use quote::{quote, ToTokens};
 use syn::punctuated::Punctuated;
-use syn::{parse_macro_input, ItemImpl};
+use syn::spanned::Spanned;
+use syn::{parse_macro_input, ExprRepeat, ItemImpl, ItemTrait, Path, TraitItemType};
 use syn::{token, Ident};
 mod change_self;
 mod dummy;
@@ -83,6 +84,113 @@ pub fn type_trait(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
         #[abstract_impl::abstract_impl(no_dummy)]
         impl #impl_name<T> for #trait_name {
             type #ty = T;
+        }
+    }
+    .into()
+}
+
+/// ```rust
+/// # use abstract_impl::use_type;
+/// #[use_type]
+/// trait SomeTrait {
+///   type Ty: Default;
+///   fn get_ty() -> Self::Ty {
+///     Self::Ty::default()
+///   }
+/// }
+/// # fn main() {}
+/// ```
+#[proc_macro_attribute]
+pub fn use_type(
+    _attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    use syn::ImplItem;
+    use syn::TraitItem;
+    use syn::Type;
+    let trait_ = parse_macro_input!(item as ItemTrait);
+    let name = trait_.ident.clone();
+    let items = trait_
+        .items
+        .clone()
+        .into_iter()
+        .filter_map(|item| match item {
+            TraitItem::Type(syn::TraitItemType {
+                attrs,
+                type_token,
+                ident,
+                generics,
+                semi_token,
+                ..
+            }) => {
+                let new_ident = Ident::new(&format!("_use_type_{ident}"), ident.span());
+                Some(Ok((
+                    ImplItem::Type(syn::ImplItemType {
+                        attrs,
+                        vis: syn::Visibility::Inherited,
+                        defaultness: None,
+                        type_token,
+                        ident,
+                        generics,
+                        eq_token: syn::token::Eq::default(),
+                        ty: Type::Path(syn::TypePath {
+                            qself: None,
+                            path: Path::from(new_ident.clone()),
+                        }),
+                        semi_token,
+                    }),
+                    new_ident,
+                )))
+            }
+            TraitItem::Const(syn::TraitItemConst {
+                attrs,
+                const_token,
+                ident,
+                generics,
+                colon_token,
+                ty,
+                semi_token,
+                ..
+            }) => {
+                let new_ident = Ident::new(&format!("_use_type_{ident}"), ident.span());
+                Some(Ok((
+                    ImplItem::Const(syn::ImplItemConst {
+                        attrs,
+                        vis: syn::Visibility::Inherited,
+                        defaultness: None,
+                        const_token,
+                        ident,
+                        generics,
+                        colon_token,
+                        ty,
+                        eq_token: syn::token::Eq::default(),
+                        expr: syn::Expr::Path(syn::ExprPath {
+                            attrs: vec![],
+                            qself: None,
+                            path: Path::from(new_ident.clone()),
+                        }),
+                        semi_token,
+                    }),
+                    new_ident,
+                )))
+            }
+            TraitItem::Fn(syn::TraitItemFn {
+                default: Some(_), ..
+            }) => None,
+            o => Some(Err(syn::Error::new(o.span(), "cannot implement functions"))),
+        })
+        .collect::<Result<(Vec<_>, Vec<_>), _>>();
+    let (items, item_names) = match items {
+        Ok(items) => items,
+        Err(err) => return err.into_compile_error().into(),
+    };
+    let impl_name = Ident::new(&format!("{name}UsingType"), name.span());
+    quote! {
+        #trait_
+        #[allow(non_camel_case_types)]
+        #[abstract_impl::abstract_impl]
+        impl #impl_name<#(#item_names),*> for #name {
+            #(#items)*
         }
     }
     .into()
